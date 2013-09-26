09026c31f5f7ce39a7380c8b9deeeab29b345dd2
Anis Moubarik <anis.moubarik@helsinki.fi>
2013-09-26 10:01:45 +0300

    sweet visual

--- javascripts/visualization.js
+++ javascripts/visualization.js
@@ -1,44 +1,43 @@
-      console.log("visualization.js loaded");
+console.log("visualization.js loaded")
 
-      DSPACE_INSTANCE = "http://ds-am2-kktest.lib.helsinki.fi/simplerest/"
+DSPACE_INSTANCE = "http://ds-am2-kktest.lib.helsinki.fi/simplerest/"
 
-      function Community(id, name) {
-        this.id = id;
-        this.name = name;
-      }
-      function Collection(id, name) {
-        this.id = id;
-        this.name = name;
-      };
-      function Item(id, name) {
-        this.id = id;
-        this.name = name;
-      };
+function Community(id, name){
+  this.id = id;
+  this.name = name;
+}
 
-function() {
+function Collection(id, name){
+  this.id = name
+  this.name = name
+}
+
+function Item(id, name){
+  this.id = id
+  this.name = name
+}
+
+(function (){
   function init(){
-          var communities = new Array();
-          var collections = new Array();
-          var items = new Array();
-          var curCol
-    //Let's start by getting rootcommunities from the DSpace instance.
+    var communities, collections, items = new Array()
+    var curCol
+
+    //Stary by getting rootcommunities from the DSpace instance
     $.getJSON(DSPACE_INSTANCE+"rootcommunities?media=json", function(data){
       $.each(data, function(index, data){
-        console.log(data.name);
         communities.push(new Community(data.id, data.name))
       });
-    }).done(function() {//Iterate through communities to save collections
+    }).done(function(){//Iterate through communities to save collections
       $.each(communities, function(index, data){
         $.getJSON(DSPACE_INSTANCE+"community/"+data.id+"/collections?media=json", function(col){
           $.each(col, function(index, col){
-            console.log(col.collectionName)
-            curCol = col;
+            curCol = col
             collections.push(new Collection(col.id, col.collectionName))
           });
-        }).done(function() {
+        }).done(function(){//Iterate through collections to save items
           $.getJSON(DSPACE_INSTANCE+"collection/"+curCol.id+"/items?media=json", function(item){
             $.each(item, function(index, item){
-              items.push(new Item(item.Id, item.metadata.filter(isTitle)[0].value));
+              items.push(new Item(item.Id, item.metadata.filter(isTitle)[0].value))
             });
           });
         });
@@ -48,5 +47,6 @@ function() {
     function isTitle(element){
       return element.element == "title" ? element : null
     }
-    $(init)
-  })();
+  }
+  $(init)
+})();
